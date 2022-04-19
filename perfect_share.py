8b3ffd4525b3cbdf01b07418797025323c658eee
Or Shahar <orshahar91@gmail.com>
2022-04-19 20:05:44 +0300

    Some fixes after code review

--- perfect_share.py
+++ perfect_share.py
@@ -1,35 +1,46 @@
-def is_prime(num):
-    if num > 1:
-        for n in range(2, num):
-            if (num % n) == 0:
+import math
+
+
+# Check whether the number is prime or not
+def is_prime(number):
+    if number > 1:
+        for divisor in range(2, int(math.sqrt(number))+1):
+            if (number % divisor) == 0:
                 return False
         return True
     else:
         return False
 
 
+# This function uses the Lucas–Lehmer primality test.
+# More information can be found here:
+# https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test
 def is_mersenne_prime(prime, mersenne_prime):
-    n = 4
+    sequence = 4
     for i in range(prime - 2):
-        n = (n * n - 2) % mersenne_prime
-    if n == 0:
+        sequence = (sequence * sequence - 2) % mersenne_prime
+    if sequence == 0:
         return True
     else:
         return False
 
 
+# In this function I used the fact that according to Euclid:
+# (2**p-1) * (2**p) - 1 is a perfect number whenever (2**p) - 1 is prime.
+# Prime numbers of the form (2**p) - 1 are called mersenne primes.
+# Additional information can be found here: https://en.wikipedia.org/wiki/Perfect_number
 def perfect_share():
-    yield 6
-    p = 3
+    yield 6  # First case to simplify the solution.
+    prime_number = 3  # The next prime number is 3 (2 yields 6).
     while True:
-        if is_mersenne_prime(p, 2 ** p - 1):
-            yield 2 ** (2 * p - 1) - 2 ** (p - 1)
+        if is_mersenne_prime(prime_number, 2 ** prime_number - 1):
+            yield 2 ** (2 * prime_number - 1) - 2 ** (prime_number - 1)
         while True:
-            p += 2
-            if is_prime(p):
+            prime_number += 2  # next potential prime number
+            if is_prime(prime_number):
                 break
 
 
 generator_iterator = perfect_share()
-for number in generator_iterator:
-    print(number)
+for perfect_number in generator_iterator:
+    print(perfect_number)
